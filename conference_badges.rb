60f95ed9b8e582aae44fdab8ecd5dfaaa9910fd3
Samuel Prada <jspradac@gmail.com>
2018-11-06 17:06:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(array)
+  badges = []
+  array.each do | luminary |
+    badges.push(badge_maker(luminary))
+  end 
+  badges 
+end 
